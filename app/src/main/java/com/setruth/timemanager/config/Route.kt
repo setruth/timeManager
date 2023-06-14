8919191352c259111bd7197e379b82c108da56bc
袜子魔 <37094964+SOCK-MAGIC@users.noreply.github.com>
2023-06-14 22:56:18 +0800

    Update Route.kt
    加点空格不会死 格式化快捷键Ctrl+Alt+L

--- app/src/main/java/com/setruth/timemanager/config/Route.kt
+++ app/src/main/java/com/setruth/timemanager/config/Route.kt
@@ -1,11 +1,11 @@
 package com.setruth.timemanager.config
 
 object APPRoute{
-    const val START_SCREEN="start"
-    const val MAIN_NAV="main_nav"
+    const val START_SCREEN = "start"
+    const val MAIN_NAV = "main_nav"
 }
 object MainNavRoute{
-    const val HOME="home"
-    const val COUNT_DOWN="count_down"
-    const val STOP_WATCH="stop_watch"
+    const val HOME = "home"
+    const val COUNT_DOWN = "count_down"
+    const val STOP_WATCH = "stop_watch"
 }
